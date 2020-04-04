b6e63287f830c8e5f4b6bde6882fadbc07e89f25
Rene K. Mueller <spiritdude@gmail.com>
2020-04-04 10:56:25 +0200

    zim-0.0.7/ZIM.pm-0.0.10: server(): resolve directs on browser-level not internally

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME=ZIM
-VERSION=0.0.9
+VERSION=0.0.10
 
 all::
 	@echo "make install deinstall edit backup git change push pull"
--- ZIM.pm
+++ ZIM.pm
@@ -8,6 +8,7 @@ package ZIM;
 #   provides basic OO interface to ZIM files as provided by kiwix.org
 #
 # History:
+# 2020/04/04: 0.0.10: in server() don't resolve direct internally, but via browser (*.stackexchange.zim rely on it)
 # 2020/04/03: 0.0.9: adding url2id cache, fixing file-not-found for server() library operation
 # 2020/03/31: 0.0.8: preliminary 64bit cluster size support to support large fulltext indexes (>4GB), improved server() web-gui supporting library (multiple zim files)
 # 2020/03/30: 0.0.7: support large article extraction direct to file (without large memory consumption) for wikipedia.zim xapian indices
@@ -370,12 +371,14 @@ sub articleById {
       my $p = $self->entry($an);
       #print to_json($self->{article},{pretty=>1,canonical=>1});
       push(@{$self->{error}},"article not found #$an"), return '' if($p<0);
-      if(defined $self->{article}->{"redirect_index"}) {
+      if(defined $self->{article}->{"redirect_index"} && !($opts && $opts->{no_redirect})) {
          $an = $self->{article}->{"redirect_index"};
       } else {
          return $opts && $opts->{metadata} ? 
             $self->{article} : 
-            $self->cluster_blob($self->{article}->{"cluster_number"}, $self->{article}->{"blob_number"}, $opts);
+            $opts && $opts->{no_redirect} && $self->{article}->{redirect_index} ? 
+               $self->{article} :
+               $self->cluster_blob($self->{article}->{"cluster_number"}, $self->{article}->{"blob_number"}, $opts);
          last;
       }
    }
@@ -515,7 +518,7 @@ sub fts {
    print "INF: #$$: xapian index $file_xapian\n" if($self->{verbose}>1);
    if(1) {
       if(!-e $file_xapian) {
-         print STDERR "WARN: #$$: no index available for $self->{header}->{file}\n" unless($self->{quiet});
+         print STDERR "WARN: #$$: no xapian index available for $self->{header}->{file}\n" unless($self->{quiet});
          return [];
       }
       my $db = Search::Xapian::Database->new($file_xapian); 
@@ -791,11 +794,21 @@ sub processRequest {
 
             print "INF: #$$: server:   serving ".($base?"$base:":"")."$url\n" if($self->{verbose});
          
-            $body = $body || $me->article($url);
+            $body = $body || $me->article($url,{no_redirect=>1});
+
+            if(ref($body) eq 'HASH') {       # -- redirect? let's do it in the browser so call content is properly referenced
+               $status = 307;
+               $me->articleById($body->{redirect_index},{no_redirect=>1});    # -- retrieve new entry, so we know the new url
+               $body = $me->{article};
+               push(@header,"Location: ".($base ? "/$base" : "") . "/$body->{namespace}/$body->{url}");
+               $body = 'redirect';
+            }
+               
             $mime = $mime || ($me->{article}->{mimetype} >= 0 ? $me->{mime}->[$me->{article}->{mimetype}] : "text/plain");
 
             if(1 && $mime eq 'text/html') {    # -- we need to change/tamper the HTML ...
                my $mh = "";
+               # $mh .= "<base href=\"/$base/\">";
                $mh .= "<style>body{margin-top:3em !important}.zim_header{z-index:1000;position:fixed;width:100%;padding:0.3em 1em;text-align:center;background:#bbb;box-shadow:0 0 0.5em 0.1em #888;top:0;left:0;text-decoration:none}.zim_header.small{font-size:0.8em;}.zim_entry{background:#eee;margin:0 0.3em;padding:0.3em 0.6em;border:1px solid #ccc;border-radius:0.3em;text-decoration:none;color:#226}.zim_entry:hover{background:#eee}.zim_entry.selected{background:#eef}.zim_search{margin:0 0.5em;padding:0.2em 0.3em;background:#ffc;border:1px solid #aa8;border-radius:0.3em;}.zim_search_input,.zim_search_input:focus{padding:0.1em 0.3em;background:none;border:none;outline:none}.zim_results{z-index:200;display:none;margin:1em 4em;padding:1em 2em;background:#fff;border:1px solid #888;box-shadow: 0 0 0.5em 0.1em #888}.zim_results.active{display:block}#_zim_results_hint{font-size:0.8em;opacity:0.7}.hit{margin-bottom:1.5em}.hit .title{font-size:1.1em;color:#00c;margin:0.25em 0;display:block;}.snippet{font-size:0.8em;opacity:0.6}.snippet .heads{font-weight:bold;font-size:0.9em;margin-top:0.5em;}.hit_icon{vertical-align:middle;height:1.5em;margin-right: 0.5em;}\@keyframes blink{0%{opacity:0}50%{opacity:1}100%{opacity:0}} .blink{animation:blink 1s infinite ease-in-out}</style>";
                $mh .= "<script>
 var _zim_base = \"$base\";
@@ -889,7 +902,12 @@ function _zim_search() {
                }
                $mh .= "</div>";
                $mh .= "<div id=_zim_results class=zim_results></div>";
+               # -- alter <body>
                $body =~ s#<body([^>]*)>#<body$1>$mh#i;
+               # -- alter <head>
+               $mh = '';
+               $mh = "<link rel=\"shortcut icon\" href=\"/$base/-/favicon\">" if($self->{catalog});
+               $body =~ s#</head>#$mh</head>#i;
             }
             
             if($me->error()) {
