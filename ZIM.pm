ecf5759d59088456eea34f0df22466810b7f4b6f
Rene K. Mueller <spiritdude@gmail.com>
2020-04-09 10:12:55 +0200

    server(): improve multiple term searches

--- ZIM.pm
+++ ZIM.pm
@@ -24,7 +24,7 @@ package ZIM;
 # 2020/03/28: 0.0.1: initial version, just using zimHttpServer.pl and objectivy it step by step, added info() to return header plus some additional info
 
 our $NAME = "ZIM";
-our $VERSION = '0.0.12';
+our $VERSION = '0.0.12.a';
 
 use strict;
 use Search::Xapian ':all';
